4a41a394c5a4f7b1c5cdca099ce6507f7ebe5078
Ari <ariannerules1@gmail.com>
2026-01-07 20:41:13 -0500

    made new classes and coded a bit of character class

---
+++ Character.java
@@ -0,0 +1,78 @@
+import javax.swing.*;
+
+
+class Character {
+   //BOOLS
+   boolean isMainCharacter;
+   boolean isInAstraea;
+
+
+   //STRINGS
+   String nameOfCharacter;
+   String nameOfSprite;
+   String tempResPath;
+
+
+   //CHAR LISTS
+   String[] MCList = {"Atlas", "Sol", "The Supreme"};
+   String[] sideSuppCharList = {""};
+
+
+   //SPRITE LISTS
+   String[] ATLSpriteList = {"Neutral", "Smile", "Sad", "Angry", "Thinking"};
+   String[] SOLSpriteList = {"Neutral", "Awaken", "Excited", "Sad", "Angry", "Thinking"};
+   String[] SUPSpriteList = {"Neutral", "Smirk", "Surprised", "Furious"};
+
+
+   ImageIcon name = new ImageIcon(tempResPath);
+
+
+   public Character(boolean isMC, String nOC, String nOS){
+       int charIndex;
+       int spriteIndex;
+       nOC = nameOfCharacter;
+       nOS = nameOfSprite;
+      
+       if (isMC){
+           charIndex = searchList(MCList, nOC);
+           if (charIndex != -1){
+               if (nameOfSprite.equalsIgnoreCase("Atlas")){
+                   spriteIndex = searchList(ATLSpriteList, nOS);
+                   if (spriteIndex != -1){
+                       tempResPath = "Character Images/MC/" + nameOfCharacter + "/" + nameOfSprite + ".png";
+                   }
+               } else if (nameOfSprite.equalsIgnoreCase("Sol")) {
+                   spriteIndex = searchList(SOLSpriteList, nOS);
+                   if (spriteIndex != -1){
+                       tempResPath = "Character Images/MC/" + nameOfCharacter + "/" + nameOfSprite + ".png";
+                   }
+               } else {
+                   spriteIndex = searchList(SUPSpriteList, nOS);
+                   if (spriteIndex != -1){
+                       tempResPath = "Character Images/MC/" + nameOfCharacter + "/" + nameOfSprite + ".png";
+                   }
+               }
+           }
+          
+       } else {
+          
+       }
+      
+      
+      
+   }
+  
+   public int searchList(String[] list, String name){
+       int index = -1;
+       for (int i = 0; i < list.length; i++){
+           if (list[i].equals(name)){
+               index = i;
+           }
+       }
+       if (index == -1){
+           System.out.println("Not found");
+       }
+      
+       return index;
+   }
+}
