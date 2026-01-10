f76e3c8477ef306fd11428deb7ad5cc684cdd03f
Ari <ariannerules1@gmail.com>
2026-01-10 09:59:33 -0500

    added animation (not yet tested if it works)

--- Display.java
+++ Display.java
@@ -1,11 +1,16 @@
+//Swing GUI
 import javax.swing.*; 
+//ActionListener and ActionEvent for JButtons and more
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+//File readers
 import java.io.File;
+//Font setup imports
 import java.io.IOException;
 import java.awt.Color; 
 import java.awt.Font;
 import java.awt.FontFormatException;
+//getContentPane() of JFrame
 import java.awt.GraphicsEnvironment;
 
 public class Display implements ActionListener{
@@ -20,7 +25,7 @@ public class Display implements ActionListener{
     JButton Option2; 
     JButton Option3; 
 
-    //JLabels
+    //JLABELS
     static JLabel astraeaLogo; 
 
     //ARRAYS
@@ -31,20 +36,25 @@ public class Display implements ActionListener{
     //IMAGEICONS
     static ImageIcon gameLogo = new ImageIcon("VIS/DECOR/Astraea.png");
 
+    //BOUNCING ANIMATION VARIABLES
+    boolean animating = true;
+    Timer animationTimer; 
+
 
     public static void main(String[] args){
+        //Creating JFrame
         GameScreen = new JFrame(); 
         GameScreen.setBounds(0, 0, 750,750);
         GameScreen.setLayout(null); 
         GameScreen.setDefaultCloseOperation(GameScreen.EXIT_ON_CLOSE);
         GameScreen.getContentPane().setBackground(Color.BLACK);
 
+        //Creating JLabel for logo in the very beginning of the game
         astraeaLogo = new JLabel();
         astraeaLogo.setBounds(0, 0, 500, 500);
         astraeaLogo.setIcon(gameLogo);
-        GameScreen.add(astraeaLogo); 
-
-
+        
+        //Set up
         Font Gerady_Bale = null;
         try {
            Gerady_Bale = Font.createFont(Font.TRUETYPE_FONT, new File("VIS/DECOR/Gerady Bale.otf"));
@@ -54,15 +64,69 @@ public class Display implements ActionListener{
            e.printStackTrace();
         }
 
+        //getContentPane() code
         GameScreen.setVisible(true);
+
+        int screenHeight = (int)GameScreen.getContentPane().getHeight();
+        int screenWidth = (int)GameScreen.getContentPane().getWidth();
+        int logoHeight = (int)gameLogo.getIconHeight(); 
+        int logoWidth = (int)gameLogo.getIconWidth(); 
+
+        astraeaLogo.setBounds((screenWidth - logoWidth)/2, (screenHeight - logoHeight)/2, logoWidth, logoHeight);
+        GameScreen.add(astraeaLogo); 
     }
 
-        public void actionPerformed(ActionEvent e) {
-        // This method is required by ActionListener interface
+    @Override
+    public void actionPerformed(ActionEvent e){
+        if (e.getSource() == start){
+            animating = false;
+            animationTimer.stop();
+        }
     }
 
     public void changeScreen(){
-            
+        //Changes the screen of the JFrame
+    }
+
+    public void upDownAnimation(JLabel label, JButton button, int range){
+        //Setting up paramet(er/re)s -> JLabel and JButton
+        label.setVisible(true);
+        label.setBounds(label.getX(), label.getY(), label.getWidth(), label.getHeight()); 
+        start = button; 
+        button.setVisible(true);
+        button.setBounds(button.getX(), button.getY(), button.getWidth(), button.getHeight()); 
+        button.setFocusPainted(false);
+        button.addActionListener(this);
+
+        final int yPosStart = label.getY();
+        final int yPosRange = range; 
+        final int[] yPosC = {yPosStart};  
+        final boolean[] movingDown = {false}; 
+
+        animating = true; 
+
+        animationTimer = new Timer(30, new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (animating) {
+                    if (!movingDown[0]){
+                        yPosC[0]--;
+
+                        if (yPosC[0] >= yPosStart + yPosRange){
+                            movingDown[0] = false; 
+                        }
+                    } else {
+                        yPosC[0]++; 
+
+                        if (yPosC[0] <= yPosStart - yPosRange){
+                            movingDown[0] = true; 
+                        } 
+                    }
+                    label.setLocation(label.getX(), yPosC[0]); 
+                }
+            }
+        });
+        animationTimer.start();
     }
 
 }
--- README.md
+++ README.md
@@ -1,29 +1,37 @@
-Welcome to ASTRAEA, a visual novel fully developped by Ari for the ICS3UR-01 (Introduction to Computer Science) summative! 
+NOTE: Please read the entire README file! It might seem long but it's worth it for the full game experience :D
 
-INSTRUCTIONS: 
+Welcome to Astraea, a visual novel fully developped by Ari for the ICS3UR-01 (Introduction to Computer Science) summative! 
+
+GAME INSTRUCTIONS: 
 - click the start button to start the story from the beginning and click the exit button to leave
 - enter your name or username into the game so we can address you! 
 - done reading before the voicelines finishes? click the text! 
 - want to review the story? click the history button! 
 - interacting with the story?: when questions are asked, click which option you'd like to respond with! 
 
-DETAILS: 
+GAME DETAILS: 
 - visual novel! inspired by doki doki literature club playstyle and umamusume career
 - interactive! you influence the story depending on your choices
 - music! all self composed
 - sound effects! stock sounds from the internet
 - visuals! all hand drawn during the december holiday break
 - Codework timeline: 
-   1. wednesday, january 7th, 2025: creating display, character, mc, sidesuppchar, options, and musicplayer classes with code in the character class
-   2. thursday, january 8th, 2025: sorting and adding files into their respective folders for easier navigation fo resources
+   1. wednesday, january 7th, 2026: creating display, character, mc, sidesuppchar, options, and musicplayer classes with code in the character class
+   2. thursday, january 8th, 2026: sorting and adding files into their respective folders for easier navigation fo resources
+   3. friday, january 9th, 2026: filling in more of the readme.md, added code into display class
+   4. saturday, january 10th, 2026: 
 
-STORY: 
-Welcome, wayward story searcher! 
-You're just in time for today's story; Astraea by Dusk Dawning! Filled with whimsicality, friendship, and love, this sappy story is accompanied by domestic rejection, tyranny, manipulation and more. 
+STORY SUMMARY / CONTEXT: 
+"Welcome, wayward travelers! Looking for a fantastical adventure?" 
 
-Don't be fooled by the book's appearance! Upon opening, the storyteller becomes the narrator as you play as Atlas, its main character. Because of of your entry into their world, your choices made throughout the story will affect the ending you see so choose wisely! You don't want to end up dead before you reach the end! 
+You stop in front of the library that has suddenly appeared before you. Just a second ago, you had missed your stop and gotten off the train at the wrong station. The sign above you read "Satyr Station" tarted walking back to your apartment, however, once you left the train, the station disappeared behind you and a singular path lit up for you to follow through the small town. Stumbling into
 
-FUTURE UPDATES: 
+"Why is reading such an adventure? It's so boring!" you may be saying, but don't be fooled by this special library's appearance! All the books in this library have been enchanted by story mages and dreamers. Upon opening, you get sucked into the books' world; the storyteller becoming the omniscient narrator as you play (mind control) as its main character. Because of of your entry into their world, your choices made throughout the story will affect the ending you see so choose wisely! You don't want to end up dead before you reach the end! 
+
+You're just in time for today's story; Astraea by Dusk Dawning! Filled with whimsicality, friendship, and love, this sappy story is accompanied by domestic rejection, tyranny, manipulation and more harsh subjects that may not be suitable for those below the age of 12. 
+
+FUTURE UPDATE PLAN: 
 - this story currently contains two different endings on one singular path -> more paths and endings to the story will be added
 - pause / save buttons in case you need to go afk
-- 
+- drawing more characters and scenes
+- voicelines for side and supporting characters too
